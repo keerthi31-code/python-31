c3528b49e22b6ea2504dffc3ea1034e62a36f9a6
keerthi <muchhakeerthi@gmail.com>
2025-12-04 23:51:54 +0530

    updated day2.py with user input examples

---
+++ day2.py
@@ -0,0 +1,19 @@
+a=15 #a value is 15
+b=10 #b value is 10
+c=a+b #addition of a+b equals to c
+print(c)
+
+#addition of two numbers by using user define data
+a=int (input("enter a value:"))
+b=int(input("enter b value: "))
+print(a+b)
+
+d=980 # d value is 980
+e=750 # e value is 750
+f=d+e # addition of d and e is equals to f
+print(f)
+
+
+d=int(input("enter d value:"))
+e=int(input("enter e value:"))
+print(d+e)
