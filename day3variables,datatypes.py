79ba9e8a72c0c1f300d045c1c483dc62088eff46
keerthi <muchhakeerthi@gmail.com>
2025-12-05 23:36:02 +0530

    variables and data types

---
+++ day3variables,datatypes.py
@@ -0,0 +1,44 @@
+#variable = value
+#a=10
+#print(a)
+
+#a,b,c=1,3,6
+#print(a,b,c)
+
+#a=1,2,3
+#print(a)
+
+#c=v=y=56
+#print(c,v,y)
+
+#abc2345=4
+#print(id(abc2345))
+
+
+'''
+Docstring for day3variables,datatypes
+int (1,-1)
+boolean
+float
+complex
+'''
+#a=1
+#b=-67
+#print(type (a),type(b))
+
+#a=True
+#b=False
+#print(type(a),type(b))
+
+#print(True==1)
+#print(False==0)
+#d=4.56
+#print(type(d))
+
+
+#k=5+7j
+#print(type(k))
+#a=2
+#print(float(a))
+b=78.5
+print(int(b))
