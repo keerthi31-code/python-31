cd10e366dab0a02a7f1cfe680f01967bc2d58884
keerthi <muchhakeerthi@gmail.com>
2025-12-07 20:39:57 +0530

    basic string operations

---
+++ day5strings.py
@@ -0,0 +1,59 @@
+'''
+strings are collection of characters
+
+'''
+#a='pythonlife'
+#b="pythonlife"
+#c='''pythonlife'''
+#print(type(a),type(b),type(c))
+
+'''
+upper
+lower
+ends with
+starts with
+replace
+find
+index
+count
+remove preffix
+remove suffix
+split
+strip
+rstrip
+lstrip
+'''
+#a="learning python"
+#print(a.upper())
+#b="LEARNING PYTHON"
+#print(b.lower())
+
+#a="learning python"
+#print(a.endswith("thon"))
+ 
+#a="learning python"
+#print(a.startswith("python"))
+
+#a="learning python"
+#print(a.replace("learning","learn"))
+
+#a="learining python"
+#print(a.find("python"))
+
+#a="learning python"
+#print(a.index("p"))
+
+#a="learning python" 
+#print(a.count("n"))
+#print(a.removeprefix("learn"))
+#print(a.removesuffix("python"))
+
+#print(a.split())
+a="  learning python  "
+print(a)
+print(len(a))
+print(a.strip())
+print(a.rstrip())
+print(len(a))
+print(a.lstrip())
+print(len(a))
