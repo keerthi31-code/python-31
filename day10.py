e25888b8d380ba264e9891cc92e31acf5eb826e4
keerthi <muchhakeerthi@gmail.com>
2025-12-13 19:57:45 +0530

    positive check

--- day10.py
+++ day10.py
@@ -5,10 +5,19 @@ elif x%6==0:
  print("B")
 else:
  print("c")
- '''
+ 
 
 marks = int(input("enter the marks:"))
 if marks >=40:
   print("pass")
 if marks >=75:
  print("distinction")
+ '''
+
+num=int(input("enter the number :"))
+if num%2==0:
+   print("positive even ")
+if num%2!=0:
+   print("positive odd")
+else:
+ print("not positive")
