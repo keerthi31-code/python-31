c561bba924a1a0745407841ea66f811be5806e1a
keerthi <muchhakeerthi@gmail.com>
2025-12-13 19:44:28 +0530

    marks

--- day10.py
+++ day10.py
@@ -1,7 +1,14 @@
-x = int(input("enter the value :"))
+'''x = int(input("enter the value :"))
 if x % 3==0:
  print("A")
 elif x%6==0:
  print("B")
 else:
  print("c")
+ '''
+
+marks = int(input("enter the marks:"))
+if marks >=40:
+  print("pass")
+if marks >=75:
+ print("distinction")
