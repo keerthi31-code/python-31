ed984aae887c18815732814f59693b2407f5fe68
keerthi <muchhakeerthi@gmail.com>
2025-12-15 19:38:05 +0530

    nested stmts

---
+++ day11.py
@@ -0,0 +1,15 @@
+number =int(input("enter the number :"))
+if number>0:
+ print("positive")
+if number%2==0:
+     print("even")
+else:
+ print("odd")
+if(number<0):
+ print("negative")
+if(number>-10):
+  print("greater than -10")
+else:
+ print("less than -10")
+if(number>0):
+  print("non negative")
