b01f858ee0c909b22a4b7d3d21028c33ecaa3920
keerthi <muchhakeerthi@gmail.com>
2025-12-20 20:01:47 +0530

    bool

--- day14NM PROBLEMS.py
+++ day14NM PROBLEMS.py
@@ -1,7 +1,21 @@
-#import math
-#x=15.75
-#result=math.modf(x) #stores the split values as tuple 
-#print(result)
+'''import math
+x=15.75
+result=math.modf(x) #stores the split values as tuple 
+print(result)
 
 y=-2.5
 print(round(y))
+'''
+#BOOLEAN FUNCTIONS
+print(bool("Hello"))
+print(bool(15))
+
+print (bool("abc"))
+print(bool(123))
+print(bool(["keerthi","navya","Aashritha"]))
+
+bool (x=12)
+if x>10:
+    print("greater than 10")
+else:
+    print("not greater than 10")
