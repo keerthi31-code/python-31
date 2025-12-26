65bd5e6c6371e7608bcbf9cc4a8988091ee264ec
keerthi <muchhakeerthi@gmail.com>
2025-12-26 23:53:38 +0530

    whileloop

---
+++ while loops.py
@@ -0,0 +1,13 @@
+# while loop is mainly concentrates on intialization, condition, increment ot decrement.
+# a while loop is used to repeatedly execute a block of code as long as a given condition is True.
+#When the condition becomes False, the loop stops, and the program continues with the next statement after the loop.
+
+i=1
+while i<=5:
+    print("python ",end="")
+    j=1
+    while j<=4:
+        print("learning ",end="")
+        j=j+1
+    print()
+    i=i+1
