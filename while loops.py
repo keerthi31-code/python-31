0334e7fc2e1ec6938193955994e12a71aecc230e
keerthi <muchhakeerthi@gmail.com>
2025-12-27 23:59:08 +0530

    whileloop

--- while loops.py
+++ while loops.py
@@ -2,12 +2,52 @@
 # a while loop is used to repeatedly execute a block of code as long as a given condition is True.
 #When the condition becomes False, the loop stops, and the program continues with the next statement after the loop.
 
-i=1
-while i<=5:
-    print("python ",end="")
-    j=1
-    while j<=4:
-        print("learning ",end="")
-        j=j+1
-    print()
-    i=i+1
+# i=1
+# while i<=5:
+#     print("python ",end="")
+#     j=1
+#     while j<=4:
+#         print("learning ",end="")
+#         j=j+1
+#     print()
+#     i=i+1
+
+# #1
+# for i in range(1,11):
+#     print(i)
+
+#2
+# i=1 
+# while i<=10:
+#     print(i)
+#     i=i+1
+
+#3
+# for i in range(1,21):
+#     if i % 2==0:
+#      print(i)
+
+#4
+# sum=0
+# i=1
+# while i<=10:
+#     sum +=i
+#     i+=1
+#     print(sum)
+
+ #5
+x=["apple","banana","guava"]  
+for i in x:
+    print(i)
+y=[19,45,87] 
+for i in y:
+    print(i)
+#6
+x=1234
+x%10==0
+while 
+print(x)
+
+
+
+
