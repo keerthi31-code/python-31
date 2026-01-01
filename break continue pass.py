dce761cb9b2d4fec40c1d3158250f1aa229af8bd
keerthi <muchhakeerthi@gmail.com>
2026-01-01 20:03:28 +0530

    break,continue,pass

---
+++ break continue pass.py
@@ -0,0 +1,30 @@
+# av=5
+
+# x= int(input("how many candies you want ?"))
+
+# i=1
+# while i<=x:
+#     if i>av:
+#         print("Out of stock")
+#         break
+#     print("candy")
+#     i+=1
+# print("Bye")    
+
+
+# for i in range(1,101):
+#     if i%3==0 or i%5==0:
+#         continue
+#     print(i)
+
+# print("Bye")    
+
+
+#pass
+for i in range(1,101):
+    if(i%2!=0):
+      pass
+
+    else:
+       print(i)
+print("bye")    
