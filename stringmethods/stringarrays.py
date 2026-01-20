355355dae43a2ff0b32a67a8da0744d1718856bb
keerthi <muchhakeerthi@gmail.com>
2026-01-20 19:56:51 +0530

    stringarrays

---
+++ stringmethods/stringarrays.py
@@ -0,0 +1,14 @@
+a="hello"
+print(a[0])
+print(a[3])
+
+b="keerthi is good girls"
+print(b[5])
+print(b[8])
+
+#looping through a string
+for x in "python":
+    print(x)
+
+for y in "learning":
+    print(y)    
