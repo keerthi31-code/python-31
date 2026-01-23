fd6e12045d768e892a25c2d79ee5ace02938560c
keerthi <muchhakeerthi@gmail.com>
2026-01-23 20:00:46 +0530

    patterns

---
+++ patterns.py
@@ -0,0 +1,27 @@
+
+#patterns 
+# print("#")
+# print("#")
+
+# print("# ",end="")
+# print("# ",end="")
+
+
+
+# for i in range(4):
+#  for j in range(4):
+#   print("# ",end="")
+#  print()
+
+#  for i in range(4):
+#    for j in range(i+1):
+#     print("#",end="")
+#    print() 
+
+# for i in range(4):
+#  for j in range(4-i):
+#   print("# ",end="")
+#  print()  
+
+
+
