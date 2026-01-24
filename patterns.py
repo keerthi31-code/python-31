d248d8d91cb64db1e12f196a6d7f0c03cd1982e5
keerthi <muchhakeerthi@gmail.com>
2026-01-24 20:03:14 +0530

    patterns

--- patterns.py
+++ patterns.py
@@ -8,28 +8,83 @@
 # Improve control over rows and columns
 
 
-print("#")
-print("#")
+# print("#")
+# print("#")
 
-print("# ",end="")
-print("# ",end="")
+# print("# ",end="")
+# print("# ",end="")
 
 
 
-for i in range(4):
- for j in range(4):
-  print("# ",end="")
- print()
+# for i in range(4):
+#  for j in range(4):
+#   print("# ",end="")
+#  print()
 
- for i in range(4):
-   for j in range(i+1):
-    print("#",end="")
-   print() 
+#  for i in range(4):
+#    for j in range(i+1):
+#     print("#",end="")
+#    print() 
 
-for i in range(4):
- for j in range(4-i):
-  print("# ",end="")
- print()  
+# for i in range(4):
+#  for j in range(4-i):
+#   print("# ",end="")
+#  print()  
 
  
+# for i in range(5):
+#     for j in range(5-i):
+#         print("* ",end="")
+#     print()   
 
+# num = int(input("enter the number:"))
+# for i in range(num):
+#     for i in range(num):
+#         print("*",end="")
+#     print()    
+
+# num=int(input("enter the number:"))
+# for i in range(num):
+#     for j in range(i+1):
+#         print("*",end="")
+#     print()    
+
+# num=int(input("enter the number:"))
+# for i in range(num):
+#     for j in range(num-i):
+#         print("*",end="")
+#     print()    
+
+
+# num=int(input("enter the number:"))
+# for i in range(1,num+1):
+#     for j in range(1, i+1):
+#         print(j,end="")
+#     print()     
+
+num=int(input("enter the number:"))
+for i in range(1,num):
+    for j in range(1,num-i+2):
+        print(j,end="")
+    print()           
+
+
+
+#for else
+# numbers=[12,7,22,46,87]
+
+# for num in numbers:
+#     if num%5==0:
+#         print(num)
+#         break
+# else:
+#     print("number not found")   
+
+#prime number in python
+# num=10
+# for i in range(2,num):
+#     if num%i==0:
+#         print("not prime")
+#         break
+# else:
+#     print("prime")
