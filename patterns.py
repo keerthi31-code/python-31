4e59adada524eaf915e2165aac46f48b7c931c0e
keerthi <muchhakeerthi@gmail.com>
2026-01-24 19:21:20 +0530

    patterns

--- patterns.py
+++ patterns.py
@@ -1,27 +1,35 @@
 
-#patterns 
-# print("#")
-# print("#")
+#patterns pattern printing programs where we use loops to print shapes using stars (*), numbers, or characters in a specific order.
 
-# print("# ",end="")
-# print("# ",end="")
-
-
-
-# for i in range(4):
-#  for j in range(4):
-#   print("# ",end="")
-#  print()
-
-#  for i in range(4):
-#    for j in range(i+1):
-#     print("#",end="")
-#    print() 
-
-# for i in range(4):
-#  for j in range(4-i):
-#   print("# ",end="")
-#  print()  
+# They are mainly used to:
+# Understand loops (for, while)
+# Practice nested loops
+# Learn logic building
+# Improve control over rows and columns
 
 
+print("#")
+print("#")
+
+print("# ",end="")
+print("# ",end="")
+
+
+
+for i in range(4):
+ for j in range(4):
+  print("# ",end="")
+ print()
+
+ for i in range(4):
+   for j in range(i+1):
+    print("#",end="")
+   print() 
+
+for i in range(4):
+ for j in range(4-i):
+  print("# ",end="")
+ print()  
+
+ 
 
