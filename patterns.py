9c1fc356a384a1955fa732de80718a311d7c0636
keerthi <muchhakeerthi@gmail.com>
2026-01-26 23:38:12 +0530

    patterns

--- patterns.py
+++ patterns.py
@@ -109,4 +109,8 @@ for i in range(1,num+1):
         print(" "*(num-i),end="")
         print("*"*(2*i-1))
       
-            
+n=5
+for i in range(1,n+1):
+        print(" "*(n-i),end="")
+        print("*"*(2*i-1))
+           
