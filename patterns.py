a031f04535d794ed54551c21e5014a51080c486e
keerthi <muchhakeerthi@gmail.com>
2026-01-28 22:15:55 +0530

    patterns

--- patterns.py
+++ patterns.py
@@ -104,13 +104,20 @@
 #         print("*",end="")
 #     print()  
 
+# num=int(input("enter the number:"))
+# for i in range(1,num+1):
+#         print(" "*(num-i),end="")
+#         print("*"*(2*i-1))
+      
+# n=5
+# for i in range(1,n+1):
+#         print(" "*(n-i),end="")
+#         print("*"*(2*i-1))
+
+
 num=int(input("enter the number:"))
 for i in range(1,num+1):
-        print(" "*(num-i),end="")
-        print("*"*(2*i-1))
-      
-n=5
-for i in range(1,n+1):
-        print(" "*(n-i),end="")
-        print("*"*(2*i-1))
-           
+    for j in range(1,i+1):
+        print(i,end="")
+    print()           
+       
