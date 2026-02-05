f30f659c98d6f4ebfb046e30ca076303999866f4
keerthi <muchhakeerthi@gmail.com>
2026-02-05 22:32:16 +0530

    added

--- lists/creatinglist.py
+++ lists/creatinglist.py
@@ -7,3 +7,23 @@ print(thislist)
 #allows duplicates 
 thislist=["apple","banana","kiwi","sapota","kiwi"]
 print(thislist) 
+
+#list length
+thislist=["apple","banana","cherry"]
+print(len(thislist))
+
+#list items - datatypes
+list1=["apple","banana"] #string
+list2=[1,3,4,5,6]#int
+list3=[True,False,True]#bool
+
+list1=["keerthi",31,True,21,"female"]
+print(list1)
+
+mylist=["apple","banana"]
+print(type(mylist))
+
+#the list() constructor when creating a newlist
+
+thislist=list(("apple","banana","cherry"))
+print(thislist)
