2ce8b4f8d1e676a1c5b014c94f83e66e95115bc5
keerthi <muchhakeerthi@gmail.com>
2026-02-05 23:05:05 +0530

    added

---
+++ lists/changelist.py
@@ -0,0 +1,17 @@
+#to change the value of a specific item, refer to the index number
+thislist=["keerthi","navya","aashritha"]
+thislist[2]="sriya"
+print(thislist)
+
+#change a range of item values
+thislist=["apple","banana","cherry","orange","kiwi","mango"]
+thislist[1:3]=["blackcurrant","watermelon"]
+print(thislist)
+
+thislist=["apple","banana","cherry"]
+thislist[1:2]=["mango","papaya"]
+print(thislist)
+
+thislist=["apple","banana","cherry"]
+thislist[1:3]=["watermelon"]
+print(thislist)
