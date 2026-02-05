4c6dbfd9a305032556035d50eb0587dc90791f32
keerthi <muchhakeerthi@gmail.com>
2026-02-05 22:47:43 +0530

    added

---
+++ lists/accesslist.py
@@ -0,0 +1,13 @@
+#list items are accessed by the index number
+thislist=["apple","banana","cherry"]
+print(thislist[0])
+
+#negative indexing
+thislist=["keerthi","aash","navya"]
+print(thislist[-1])
+
+#range of index
+thislist=["keerthi","manasa","navya","aashritha","sriya","narendra","pranay"]
+print(thislist[2:4])#index start with included 2 and 4 not included
+print(thislist[:4])
+print(thislist[2:])
