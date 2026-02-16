eca4c549f5468e328ea72f61416cee14f18274f1
keerthi <muchhakeerthi@gmail.com>
2026-02-16 23:27:36 +0530

    added

---
+++ sets/joinsets.py
@@ -0,0 +1,11 @@
+#there are several ways to join two or more sets 
+#union() method returns a newset with all items from both sets
+set1={"a","b","c"}
+set2={1,2,3,4}
+set3=set1.union(set2)
+print(set3)
+ # "|" operator is used to instead of union()
+set1={"keerthi","navya"}
+set2={15,41} 
+set3=set1|set2
+print(set3)
