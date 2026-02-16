03ee025e777eae5ab78a1096da7be93e84f43bac
keerthi <muchhakeerthi@gmail.com>
2026-02-16 23:20:27 +0530

    added

---
+++ sets/loopsets.py
@@ -0,0 +1,8 @@
+#by using for loop we can loop the items in set
+thisset={"amla","beetroot","lemon"}
+for x in thisset:
+    print(x)
+
+thisset={"keerthi","navya","aashritha","akhila","pavani","keerthi","harshitha"}
+for x in thisset:
+ print(x)
