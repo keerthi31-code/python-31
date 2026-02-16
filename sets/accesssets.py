a8aebcca85eceabdf26c60a858943585e5958281
keerthi <muchhakeerthi@gmail.com>
2026-02-16 19:21:25 +0530

    added

---
+++ sets/accesssets.py
@@ -0,0 +1,5 @@
+#it cannot access items in a set by giving index or key
+#by using for loop  and for a specified value in a set using in keyword
+thisset={"apple","banana","kiwi"}
+for x in thisset:
+    print(x)
---
+++ sets/sets.py
@@ -0,0 +1,7 @@
+#sets are used to store multiple items in a single variable
+myset={"a","b","c"}
+print(myset)
+#set items are unchangeable, but can remove items and add new items
+#sets are unordered , so the output will be in any order
+thisset={"navya","aashritha","mansa","sriya"}
+print(thisset)
