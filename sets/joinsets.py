87b80477c7518960d7509a8a8e41d81ea7865125
keerthi <muchhakeerthi@gmail.com>
2026-02-17 14:43:54 +0530

    added

--- sets/joinsets.py
+++ sets/joinsets.py
@@ -90,3 +90,22 @@ set2 = {"google", "microsoft", "apple"}
 set3 = set1.symmetric_difference(set2)
 
 print(set3)
+
+# symmetric_difference() 
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.symmetric_difference(set2)
+
+print(set3)
+# the ^ operator instead of the symmetric_difference() method, and you will get the same result.
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1 ^ set2
+print(set3)
+#Use the symmetric_difference_update() method to keep the items that are not present in both sets:
+set1={"apple","banana","cherry"}
+set2={"google","microsoft","apple"}
+set1.symmetric_difference_update(set2)
+print(set1)
