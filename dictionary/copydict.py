cb13ced39591e13effcaf15bee636bda9f75e4ab
keerthi <muchhakeerthi@gmail.com>
2026-02-19 22:56:51 +0530

    added

---
+++ dictionary/copydict.py
@@ -0,0 +1,14 @@
+thisdict={
+    "name":"keerthi",
+    "age":21
+}
+mydict=thisdict.copy()
+print(mydict)
+
+# dict()
+thisdict={
+    "name":"keerthi",
+    "age":21
+}
+mydict=dict(thisdict)
+print(mydict)
