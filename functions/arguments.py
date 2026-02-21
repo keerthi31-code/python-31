c9b772ce4cca00adffa39827113199b9fc068c4e
keerthi <muchhakeerthi@gmail.com>
2026-02-21 22:22:12 +0530

    added

---
+++ functions/arguments.py
@@ -0,0 +1,16 @@
+# Arguments information can be passed into functions as arguments
+# Arguments are specified after the function name, inside the parentheses. 
+# can add as many arguments as want, just separated by comma
+def my_function(fname):
+    print(fname + "csma")
+my_function("keerthi")
+my_function("navya")
+my_function("aash")
+
+#parameter vs Arguments 
+#A parameter is the variable listed inside the parentheses in the function definition.
+#An argument is the actual value that is sent to the function when it is called.
+
+def my_function(name):
+    print("hello", name)
+my_function("keerthi")    
