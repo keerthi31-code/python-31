3361be21a587875b0453d303c0fb1a01147f7a94
keerthi <muchhakeerthi@gmail.com>
2026-03-02 23:27:48 +0530

    added

--- functions/func.py
+++ functions/func.py
@@ -106,5 +106,12 @@ def factorial(n):
 print(factorial(5))    
     
      
+def sum_of_digits(n):
+    result =0
+    while  n>0:
+        result+=n%10   
+        n=n//10
+    return result
+print(sum_of_digits(123))      
 
     
