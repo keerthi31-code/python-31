5090d0dc1e9c6e4027521452b494e46424c304b5
keerthi <muchhakeerthi@gmail.com>
2026-03-12 22:35:31 +0530

    added

--- functions/args and kwargs.py
+++ functions/args and kwargs.py
@@ -6,5 +6,14 @@ print(add(5,6))
 #if we want to add many numbers that's where *args used
 def add(*numbers):
     print(numbers)
-add(1,2,3,4,5)    
+add(1,2,3,4,5)    #*args stores arguments in tuple
+
+
+def add(*numbers):
+    sum=0
+    for i in numbers:
+        sum+=i
+    print(sum)
+add(1,2,3,4,5)        
+
     
