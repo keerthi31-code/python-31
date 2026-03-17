d1eb1d3c2b25a2e254d5616be79bc3078128fe25
keerthi <muchhakeerthi@gmail.com>
2026-03-17 15:26:07 +0530

    added

--- functions/scope.py
+++ functions/scope.py
@@ -1,6 +1,11 @@
 # scope means where a variable can be accessed in a program
  # python mainly has 4 types LEDB Rule
-#local, enclosing,global,builtin
+#local, 
+# enclosing,
+# global,
+# builtin
+
+
 #local scope
 #a variable can be used only inside that function
 def fun():
@@ -15,6 +20,49 @@ x=20
 def fun():
    print(x)
 fun()    
-x=30
-def fun():print(x)
-fun()
+
+
+#modifying global variables
+#to change the global variable inside a function, must use the keyword
+# global
+x=10
+def change():
+    global x 
+    x=50
+change()
+print(x)   
+
+str="keerthi"
+def change():
+    global str
+    str ="manasa"
+change()
+print(str)    
+#without global , python will treat it as a new local variable
+
+#enclosing scope -- a function inside another function can access the outer function variable
+def outer():
+    x=10
+    def inner():
+        print(x)
+    inner()
+outer()        
+
+def outer():
+    str="keerthi"
+    def inner():
+        print(str)
+    inner()
+
+outer()  # here str belongs to the outer function
+
+
+# # built-in scope
+# print()
+# len()
+# max()
+# min()
+# sum()
+x="keerthi"
+print(len(x))
+
