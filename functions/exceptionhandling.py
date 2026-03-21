c50084403dee9efff2e7deb8cae6da07d1b11d2f
keerthi <muchhakeerthi@gmail.com>
2026-03-21 23:19:52 +0530

    added

--- functions/exceptionhandling.py
+++ functions/exceptionhandling.py
@@ -1,6 +1,23 @@
 # exception handling -- to avoid program crashes
-try:
-    a=int(input())
-    print(10/a)
-except: 
-    print("error occured")
+# try:
+#     a=int(input())
+#     print(10/a)
+# except: 
+#     print("error occured")
+
+# QUESTIONS
+
+def fact(n):
+    if n==1:
+        return 1
+    return n*fact( n-1)
+print(fact(5))
+
+def sum(n):
+    if n==1:
+        return 1
+    return n+sum(n-1)
+                #5+4, 4+3, 3+2, 2+1, 1+0
+
+print(sum(5))
+
