fbaf6c947df81171458e43a6cd26317e352bd17a
keerthi <muchhakeerthi@gmail.com>
2026-03-23 23:19:43 +0530

    added

--- functions/exceptionhandling.py
+++ functions/exceptionhandling.py
@@ -38,15 +38,37 @@ def reverse_string(n):
 
 print(reverse_string("keerthi"))   
 
-def fib(n):
-    if n==1:
+# def fib(n):
+#     if n==1:
+#         return 0
+#     if n==2:
+#          return 1  
+#     return fib(n-1)+fib(n-2)
+# n=int(input("enter the number: "))
+# for i in range(1,n+1):
+#     print(fib(i), end=" ")
+
+
+
+def sum_numbers(n):
+    sum=0
+    for i in range(1,n+1):
+        sum=sum+i
+    return sum
+print(sum_numbers(5))  
+
+def sumDigits(n):
+    if n==0:
         return 0
-    if n==2:
-         return 1  
-    return fib(n-1)+fib(n-2)
-n=int(input("enter the number: "))
-for i in range(1,n+1):
-    print(fib(i), end=" ")
+    return n%10+sumDigits(n//10)
+print(sumDigits(123))
 
+def sum_digits(n):
+    total = 0
+    while n > 0: #123>0
+        total += n % 10 # 123%10
+        n = n // 10 # 123=123//10
+    return total
 
+print(sum_digits(123))
     
