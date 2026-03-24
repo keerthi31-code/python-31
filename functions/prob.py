259f1e6181590d91f5e134d94df06819c9f51ccc
keerthi <muchhakeerthi@gmail.com>
2026-03-24 23:23:44 +0530

    added

---
+++ functions/prob.py
@@ -0,0 +1,11 @@
+n=int(input("enter the value: "))
+if n & 1:
+    print("odd")
+else: 
+    print("even")  
+
+for i in range(1,6):
+    if i &1:
+        print(i,"odd") 
+else:
+    print(i, "even")     
