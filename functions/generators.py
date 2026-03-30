6c0ad46a17c3838fb175cfc29ba45ba666fcd965
keerthi <muchhakeerthi@gmail.com>
2026-03-30 23:46:34 +0530

    added

--- functions/generators.py
+++ functions/generators.py
@@ -6,3 +6,34 @@ def my_generator():
     yield 3
 for value in my_generator():
     print(value)    
+
+ #generators allow you to iterate over data without storing the entire dataset in memory
+ # instead of using return, generator use the yiels keyword
+
+
+#the yield keyword keyword
+# yield keyword is what makes a function a generator
+# when yield is encountered, the function's state is saved, and the value is returned.
+# the next time the generator is called, it continues from where it left off
+
+## generator that yields numbers
+def count_upto(n):
+    count=1
+    while count<=n:
+        yield count
+        count +=1
+for num in count_upto(5):
+    print(num)        
+
+# unlike generators which terminates the function, yield pauses it and can be called multple times
+
+#GENERATORS SAVES MEMORY
+#generators are memory efficient because they generate values on-the-fly instead of storing everything in memery
+def large_sequence(n):
+    for i in range(n):
+        yield i
+gen = large_sequence(1000000) 
+print(next(gen))
+print(next(gen))
+print(next(gen))
+       
