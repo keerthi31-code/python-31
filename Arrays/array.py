fe4fe7b76fbfecbb83c0e02b9fd01df06e72d899
keerthi <muchhakeerthi@gmail.com>
2026-04-13 23:20:14 +0530

    added

---
+++ Arrays/array.py
@@ -0,0 +1,8 @@
+# What is an array
+# array is a special varibale, which can hold more than one value at a time
+#access the elements of an array
+cars=["Ford","Volvo","BMW"]
+x=cars[0]
+#modify the value of the first array item
+cars[0]="toyota"
+print(cars)
