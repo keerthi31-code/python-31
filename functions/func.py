e2434834be1f66a6eae98092cfd5a0e3ccee8c31
keerthi <muchhakeerthi@gmail.com>
2026-04-22 23:27:17 +0530

    added

--- functions/func.py
+++ functions/func.py
@@ -248,6 +248,14 @@ def count_odd(lst):
     return count
 print(count_odd([1,2,3,4,5,6,7])) 
 
+def sumOfMultiples(n):
+        total = 0
+        
+        for i in range(1, n + 1):
+            if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
+                total += i
+        
+        return total
 
 
 
