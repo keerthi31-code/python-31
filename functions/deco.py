bb32ec8a9d6349d3ca45f4b32504cdd2d48ade75
keerthi <muchhakeerthi@gmail.com>
2026-04-23 23:45:35 +0530

    added

--- functions/deco.py
+++ functions/deco.py
@@ -162,5 +162,12 @@ a,b=map(int,input("enter thr nums:").split())
 add(a,b)
 sub(a,b)
 
-
+def titleToNumber(columnTitle):
+        result = 0
+        
+        for ch in columnTitle:
+            value = ord(ch) - ord('A') + 1
+            result = result * 26 + value
+        
+        return result
         
