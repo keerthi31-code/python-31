de88cc91fb61073f997f0957e0770b9b442c930c
keerthi <muchhakeerthi@gmail.com>
2026-04-29 23:12:58 +0530

    added

--- prob.py
+++ prob.py
@@ -1,28 +1,5 @@
-def countvowels(s):
-    count=0
-    for ch in s:
-        if ch in "aeiou":
-            count +=1
-    return count
-print(countvowels("lasya"))
-print(countvowels("keerthi"))
-print(countvowels("navya"))
 
-def remove_duplicates(n):
-    result=[]
-    
-    for i in n:
-        if i not in result:
-            result.append(i)
-    return result
-print(remove_duplicates([1,2,3,4,5,5,6,7,8,8,9]))        
-
-
-def remove_duplicates(n):
-    result=[]
-    
-    for i in n:
-        if i not in result:
-            result.append(i)
-    return result
-print(remove_duplicates([1,4,4,6,7,8,8]))        
+class student:
+    def __init__(self):
+        print("student is good")
+c1=student()       
