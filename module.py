7b1ef546bb1f5eaa5b1351cc746df59744277707
keerthi <muchhakeerthi@gmail.com>
2026-04-30 23:30:44 +0530

    added

--- module.py
+++ module.py
@@ -1,4 +1,14 @@
 def greeting(name):
     print("hello" + name)
 import module
-module.greeting(" keerthi")    
+module.greeting(" keerthi")   
+
+#variables in modules
+person1={
+    "name":"keerthi",
+    "age":21,
+    "country": "india"
+}
+import module
+a=module.person1["age"]
+print(a)
