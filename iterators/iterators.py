81dc12ecf774d5d6696e96833961be3ef44b6765
keerthi <muchhakeerthi@gmail.com>
2026-04-30 23:15:44 +0530

    added

--- iterators/iterators.py
+++ iterators/iterators.py
@@ -39,3 +39,49 @@ mytuple=["keerthi",20,"Python"]
 for x in mytuple:
     print(x)  
 
+
+#create an iterator
+#the __iter__() method acts similar,
+# you can do operations (initializing etc.),
+#  but must always return the iterator object itself.
+
+#The __next__() method also allows
+#  to do operations, and must return the next item in the sequence.
+
+class mynumbers:
+    def __iter__(self):
+        self.a=1
+        return self
+    def __next__(self):
+        x=self.a
+        self.a+=1
+        return x
+myclass=mynumbers()
+myiter=iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
+#stop iteration 
+#To prevent the iteration from going on forever, we can use the StopIteration statement.
+
+#In the __next__() method, 
+# we can add a terminating condition to raise an error if the iteration is done a specified number of times:
+class mynumber:
+    def __iter__(self):
+        self.a=1
+        return self
+    def __next__(self):
+        if self.a<=10:
+            x=self.a
+            self.a+=1
+            return x
+        else:
+            raise StopIteration
+myclass=mynumber()
+myiter=iter(myclass)
+for x in myiter:
+    print(x)        
