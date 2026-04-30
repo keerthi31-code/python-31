cef43d814cc9eead595a0f26c8aa4c2fbd3273e3
keerthi <muchhakeerthi@gmail.com>
2026-04-30 20:58:35 +0530

    added

--- prob.py
+++ prob.py
@@ -3,3 +3,17 @@ class student:
     def __init__(self):
         print("student is good")
 c1=student()       
+
+
+class employee: #parent class
+    def details(self):
+        print("i'm employee")
+
+class developer(employee): #child class
+    def coding(self):
+        print("i'm developer")
+        #access parent class from child class
+d1=developer()
+d1.details()
+d1.coding()
+
