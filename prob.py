34e1254eb256dd462c02391c08260c2cb2e4dc2d
keerthi <muchhakeerthi@gmail.com>
2026-05-01 20:58:18 +0530

    added

--- prob.py
+++ prob.py
@@ -39,7 +39,7 @@ class child(parent):
         print("im children") 
 
 c1=child()
-c1.children()    
+c1.parent()    
 
 # multiple inheritance -- multiple parent , one child
 class parent1:
@@ -53,4 +53,33 @@ class child(parent1, parent2):
         print("im children")                
 c1=child()
 c1.mother()
-c1.father()        
+c1.father()     
+
+# multilevel -- child class, parent class, grand parent class
+class grandparent:
+    def grandpa(self):
+        print("im grand pa")
+class parent(grandparent):
+    def parent(self):
+        print("im father")
+class child(parent):
+    def children(self):
+        print("im children")     
+c1=child()
+c1.parent()
+c1.grandpa()    
+
+# hierarical --one parent multiple childrens
+class parent:
+    def father(self):
+        print("im father")
+class child1(parent):
+    def children1(self):
+        print("im elder child")
+class child2(parent):
+    def children2(self):
+        print("im child2 ")    
+c1=child1()
+c1.father()
+c2=child2()
+c2.father()
