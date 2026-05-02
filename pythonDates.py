fb3cfb488112a7d380f2d204d5a0ef155a99989d
keerthi <muchhakeerthi@gmail.com>
2026-05-02 12:40:51 +0530

    added

---
+++ pythonDates.py
@@ -0,0 +1,17 @@
+# python Dates -- date in python is not a data type of its own, but it can import a 
+# module named datetime to work with dats as date object
+import datetime
+x=datetime.datetime.now()
+print(x)
+
+import datetime
+x=datetime.datetime.now()
+print(x.year)
+print(x.strftime("%A")) # returns the day of week
+
+# creating Date objects
+# to create a date, we can use the dattime() class(constructor) of datetime module
+# datetime()  class requires yr,month,day to create a date
+import datetime
+x=datetime.datetime(2026,5,2)
+print(x)
