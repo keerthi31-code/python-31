d97320c4b6f0129611824012ba603fbdc82b1165
keerthi <muchhakeerthi@gmail.com>
2026-05-02 12:24:47 +0530

    added

--- module.py
+++ module.py
@@ -12,3 +12,22 @@ person1={
 import module
 a=module.person1["age"]
 print(a)
+
+#naming a module -- name the module file whatever, but it must have the file extension .py
+# renaming a module -- create an alias when import a module by using as keyword
+
+import module as x
+a=x.person1["age"]
+print(a)
+
+#built-in modules
+#platform module
+import platform 
+x=platform.system()
+print(x)
+
+#using dir() function -- to list all the function names in a module
+# the dir() func
+import platform
+x=dir(platform)
+print(x)
