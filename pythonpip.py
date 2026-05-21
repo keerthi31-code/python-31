8949239f7ea53dc5dc86c8eb50446327abf6e02e
keerthi <muchhakeerthi@gmail.com>
2026-05-21 23:16:05 +0530

    added

--- pythonpip.py
+++ pythonpip.py
@@ -8,4 +8,48 @@ c= camelcase.CamelCase()
 txt="hello keerthi"
 print(c.hump(txt))
 
-# remove package
+# remove package -- hence removed
+
+# list installed package -- pip list
+
+# upgrade package 
+
+# install specific vesrsion
+#pip install numpy==1.26.4
+
+# showing package details
+# pip show numpy
+
+# save project packages
+# pip freeze > requirements.txt
+# numpy=1.26.4
+#pandas==2.2.2
+
+
+# where packages come from: PyPI(python package index)
+# it is offcial python package website
+
+# common beginer errors
+# pip is not recognized
+
+# we can fix by -- python -m pip install package_name
+
+
+''' 
+difference b/w module and package 
+module .py file eg: math.py
+package is folder containing many modules
+numpy/
+
+Most Used PIP Commands (Quick Revision)
+Command	Purpose
+pip --version	-check pip
+pip install package	 -install
+pip uninstall package	-remove
+pip list	-list packages
+pip show package	-package details
+pip freeze	-save dependencies
+pip install -r file.txt -install from file
+pip install --upgrade package	-update
+
+'''
