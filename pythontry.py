4484df8a90f5dc802ee4bd7a4ba1e87970996646
keerthi <muchhakeerthi@gmail.com>
2026-05-25 23:34:57 +0530

    added

---
+++ pythontry.py
@@ -0,0 +1,19 @@
+'''
+Python Try Except
+the try block is used to test a block of code for errors
+except block -- handle the errors
+else block -- execute code when there is no error
+finally -- execute code, regardless of the result of the try -- escept blocks
+
+'''
+"""
+Exception Handling
+error means exception , when exception occurs python will normally stop and generate an error message
+this exception can be handled using the try statement
+
+"""
+
+try:
+    print(x)
+except:
+    print("An Exception occured")    
