eb046855b27450ebd0db84592c431ce5156edeb6
keerthi <muchhakeerthi@gmail.com>
2026-05-29 23:32:06 +0530

    added

--- matrix.py
+++ matrix.py
@@ -10,3 +10,39 @@ for i in range(rows):
         total[i][j]=a[i][j]+b[i][j]
     print(total)    
 
+a1=[2,3,4],[5,6,8],[10,11,12]
+b1=[2,3,4],[5,6,8],[10,11,12]
+res=[0,0,0],[0,0,0],[0,0,0]
+row=3
+col=3
+for i in range(row):
+    for j in range(col):
+        res[i][j]=a1[i][j]-b1[i][j]
+print(res)
+
+
+
+
+
+
+
+
+
+
+
+a=[[1,2,3],
+   [2,3,4],
+   [3,4,5]]
+
+b=[[1,2],
+   [2,3],
+   [3,4]]
+
+result=[]
+for i in range(len(a)):          # here len(a) is an row                                   
+    row=[0]*len(b[0])            # here len(b[0])  is no of colums -->  if confuse refer matrix-1.py
+    for j in range(len(b[0])):      
+        for k in range(len(b)):
+            row[j]+=a[i][k]*b[k][j]
+    result.append(row)
+print(result)            
