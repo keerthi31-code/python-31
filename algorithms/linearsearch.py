2c442e6b042bc97bb477f133958083f1688cebca
keerthi <muchhakeerthi@gmail.com>
2026-06-04 16:06:42 +0530

    added

--- algorithms/linearsearch.py
+++ algorithms/linearsearch.py
@@ -5,14 +5,22 @@
 #         print("index:",i)
 
 
-arr=list(map(int,input().split()))
-k=int(input())
-for i in range(len(arr)):
-    if arr[i]==k:
-        print("index:",i)
+# arr=list(map(int,input().split()))
+# k=int(input())
+# for i in range(len(arr)):
+#     if arr[i]==k:
+#         print("index:",i)
    
-arr=list(map(int,input().split()))
-a=int(input())
-for i in range(len(arr)):
-    if arr[i]==a:
-        print(i)
+# arr=list(map(int,input().split()))
+# a=int(input())
+# for i in range(len(arr)):
+#     if arr[i]==a:
+#         print(i)
+
+arr=list(map(int,input().split("," )))
+b=int(input("Target:"))
+count=0
+for num in arr:
+    if num >1:
+        count+=1
+print(count)    
