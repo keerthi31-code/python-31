ba4f46610e2d4ad5459276a984fc740b049f1ff2
keerthi <muchhakeerthi@gmail.com>
2026-06-04 16:53:40 +0530

    added

--- algorithms/linearsearch.py
+++ algorithms/linearsearch.py
@@ -28,10 +28,14 @@
 
 arr=[4,7,2,9,1]
 num=0 #num = 0
-for i in arr: #i=4,7,2
-    if i > num: #4>0,7>4,4>2 -- true
+for i in arr: #i=4,7,2,9,1
+    if i > num: #4>0 ,7>4 -- true ,9>7 true,4>2, i=1 1>9 no -- no 
         num=i # num 4 
                 # num=7
+                # num=7
+                #num=9
+                #num remains same num=9
+
 print("largestno.",num)
 
 lst=[4,7,2,9,1]
@@ -39,5 +43,18 @@ num=lst[0]
 for i in lst:
     if i < num:
         num=i
-print(num)        
+print(num)   
 
+
+
+lst=[1,2,3,4,5,6]
+num=int(input())
+found=False
+for i in lst:
+    if i==num:
+        found = True
+
+if found:
+    print("yes")
+else:
+    print("no")             
