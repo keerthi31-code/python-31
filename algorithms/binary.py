c49e3c96567665d06e5d7c3e7ada733c2e201b2f
keerthi <muchhakeerthi@gmail.com>
2026-06-06 16:00:49 +0530

    added

--- algorithms/binary.py
+++ algorithms/binary.py
@@ -1,28 +1,39 @@
-arr=list(map(int,input().split()))
-t=int(input("enter the value:"))
-l=0
-r=len(arr)-1
-while l<=r:
-    m=l+r//2
-    if arr[m]==t:
-        print(m)
-        break
-    elif arr[m]<t:
-        l=m+1  
-    else:
-        r=m-1      
+# arr=list(map(int,input().split()))
+# t=int(input("enter the value:"))
+# l=0
+# r=len(arr)-1
+# while l<=r:
+#     m=l+r//2
+#     if arr[m]==t:
+#         print(m)
+#         break
+#     elif arr[m]<t:
+#         l=m+1  
+#     else:
+#         r=m-1      
 
 
-arr=list(map(int,input().split(",")))
-t=int(input("Target:")) 
-l=0
-r=len(arr)-1
-while l<=r:
-    m=(l+r)//2
-    if arr[m]==t:
-        print(m)
-        break
-    elif arr[m]<t:
-        l=m+1
-    else:
-        r=m-1    
+# arr=list(map(int,input().split(",")))
+# t=int(input("Target:")) 
+# l=0
+# r=len(arr)-1
+# while l<=r:
+#     m=(l+r)//2
+#     if arr[m]==t:
+#         print(m)
+#         break
+#     elif arr[m]<t:
+#         l=m+1
+#     else:
+#         r=m-1    
+
+
+arr=[1,2,5,6,7,4] 
+t=int(input("target:"))
+for i in range(len(arr)-1):
+    if i in arr:
+        t==i
+    print("yes")
+    break 
+else:
+    print("no")          
