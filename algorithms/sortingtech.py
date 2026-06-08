60ed0271adf7bc099c52d70534d7e7a2a0de6d42
keerthi <muchhakeerthi@gmail.com>
2026-06-08 23:06:21 +0530

    added

--- algorithms/sortingtech.py
+++ algorithms/sortingtech.py
@@ -1,14 +1,22 @@
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):#6
-        for j in range(0, n-i-1):#6-1-1=4
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
+# def bubble_sort(arr):
+#     n = len(arr)
+#     for i in range(n):#6
+#         for j in range(0, n-i-1):#6-1-1=4
+#             if arr[j] > arr[j+1]:
+#                 arr[j], arr[j+1] = arr[j+1], arr[j]
+#     return arr
     
-arr=[5, 3, 8, 1, 2]
-result=bubble_sort(arr)
- 
-    
-print(result)
+# arr=[5, 3, 8, 1, 2]
+# result=bubble_sort(arr)   
+# print(result)
 
+def sorting_ele(arr):
+    n=len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j]> arr[j+1]:
+                arr[j],arr[j+1]= arr[j+1],arr[j]
+    return arr
+arr=[7,5,3,8,1]
+result=sorting_ele(arr)
+print(result)            
