c669cf44472b192e801fc92ecfca572bc0509ba4
keerthi <muchhakeerthi@gmail.com>
2026-06-11 23:16:55 +0530

    added

--- algorithms/sortingtech.py
+++ algorithms/sortingtech.py
@@ -34,11 +34,11 @@ arr=[7,2,9,1]
 result=sort_des(arr)
 print(result) 
 
-
+# count swaps
 def sort_count(arr):
     n=len(arr)
     count=0
-    for i in range(n):
+    for i in range(n-1):
         for j in range(0,n-i-1):
             if arr[j]>arr[j+1]:
                 arr[j],arr[j+1]=arr[j+1],arr[j]
@@ -46,4 +46,13 @@ def sort_count(arr):
     return count
 arr=[4,3,2,1] 
 result=sort_count(arr)
-print(result)           
+print(result)   
+
+def sorted_array(arr):
+    n=len(arr)
+    for i in range(n-1):
+            if arr[i]>arr[i+1]:
+                return "not sorted"
+    return "sorted"
+arr=list(map(int,input().split()))
+print(sorted_array(arr))    
