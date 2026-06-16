f7f59a0626f01c3d71986264aa6482d12cdbd5bc
keerthi <muchhakeerthi@gmail.com>
2026-06-16 23:25:39 +0530

    added

---
+++ algorithms/insertion.py
@@ -0,0 +1,14 @@
+def insertion_sort(arr):
+    n=len(arr)
+    for i in range(1,n):
+        key=arr[i]
+        j=i-1
+        while j>=0 and arr[j]>key:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=key
+    return arr
+arr=insertion_sort([5,4,2,1,6,3])
+print(arr) 
+
+
