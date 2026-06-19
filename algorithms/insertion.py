17af703c9ad724e930d25e89f467495595955c47
keerthi <muchhakeerthi@gmail.com>
2026-06-20 00:00:17 +0530

    added

--- algorithms/insertion.py
+++ algorithms/insertion.py
@@ -95,4 +95,19 @@ if sorted(arr):
 else:
     print("not sorted")       
 
+def k_sort(arr,k):
+    n= len(arr)
+    for i in range(1,len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] >key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+        if i ==k:
+            return arr
+    return arr    
+arr=[1,5,3,7,8,4]
+k=2
+print(k_sort(arr, k))
 
