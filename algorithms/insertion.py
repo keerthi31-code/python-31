758d555d3656960479083993b66d28191db3ed17
keerthi <muchhakeerthi@gmail.com>
2026-06-21 22:40:57 +0530

    added

--- algorithms/insertion.py
+++ algorithms/insertion.py
@@ -139,3 +139,19 @@ def sort_steps(arr):
     return arr
 arr=list(map(int,input().split()))
 print(sort_steps(arr))
+
+def count_shifts(arr):
+    n=len(arr)
+    shifts=0
+    for i in range(1,n):
+        key=arr[i]
+        j=i-1
+        while j>=0 and arr[j]>key:
+            arr[j+1]=arr[j]
+            shifts+=1
+            j-=1
+        arr[j+1]=key
+        
+    return shifts
+arr=list(map(int,input().split()))
+print(count_shifts(arr))
