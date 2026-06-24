b103d9a7ca71720e7074c9572e41f6e953770422
keerthi <muchhakeerthi@gmail.com>
2026-06-24 23:03:12 +0530

    added

--- algorithms/merge.py
+++ algorithms/merge.py
@@ -4,20 +4,13 @@ def merge_sort(arr):# [8,6,4,9,1,2]
     r = n-1
     if n>1:
         mid = (l+r)//2 
-
         left = arr[:mid+1]
         right = arr[mid+1:]
-
         #print("before:",left, right)
-
         merge_sort(left)
         merge_sort(right)
-
         #print("after", left, right)
-
-
         i=j=k=0
-
         while i< len(left) and j < len(right):
             if left[i]<right[j]:
                 arr[k] = left[i]
@@ -25,9 +18,7 @@ def merge_sort(arr):# [8,6,4,9,1,2]
             else:
                 arr[k]=right[j]
                 j+=1
-
             k+=1
-
         while i< len(left):
             arr[k] = left[i]
             i+=1
@@ -36,9 +27,40 @@ def merge_sort(arr):# [8,6,4,9,1,2]
             arr[k] = right[j]
             j+=1
             k+=1
-
-    return arr
-  
-    
+    return arr 
 a = [8,6,4,9,1,2]
 print(merge_sort(a))
+
+def mergeSort(arr):
+    n=len(arr)
+    l=0
+    r=n-1
+    if n>1:
+        mid=(l+r)//2
+        left=arr[:mid+1]
+        right=arr[mid+1:]
+        mergeSort(left)
+        mergeSort(right)
+        i=j=k=0
+        while i<len(left) and j<len(right):
+            if left[i]<right[j]:
+                arr[k]=left[i]
+                i+=1
+            else:
+                arr[k]=right[j]
+                j+=1
+            k+=1
+        while i<len(left):
+            arr[k]=left[i]
+            i+=1
+            k+=1
+        while j<len(right):
+            arr[k]=right[j]
+            j+=1
+            k+=1
+    return arr
+arr=[4,6,2,3,1,8,9]
+print(mergeSort(arr))
+
+
+
