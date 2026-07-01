ce4862419924d87708fb77e4dee5bbfd67a10bf6
keerthi <muchhakeerthi@gmail.com>
2026-07-01 22:56:01 +0530

    added

--- algorithms/merge.py
+++ algorithms/merge.py
@@ -163,3 +163,26 @@ def merge_s(arr1,arr2):
 arr1=[1,3,5]
 arr2=[2,4,6] 
 print(merge_s(arr1,arr2))  
+
+def mergeSort(left, right):
+    result=[]
+    i,j=0,0
+    while i<len(left) and j<len(right):
+        if left[i]<=right[j]:
+            result.append(left[i])
+            i+=1
+        else:
+            result.append(right[j])
+            j+=1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+def merge_sort(arr):
+    if len(arr)<=1:
+        return arr
+    mid=len(arr)//2
+    left=merge_sort(arr[:mid])
+    right=merge_sort(arr[mid:])
+    return merge(left,right)
+arr=[4,5,3,2,5,4]
+print(merge_sort(arr))
