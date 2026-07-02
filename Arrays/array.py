fd6fc2dc1aed1244580c065e0017bd2192bac56f
keerthi <muchhakeerthi@gmail.com>
2026-07-02 22:16:36 +0530

    added

--- Arrays/array.py
+++ Arrays/array.py
@@ -48,3 +48,29 @@ names.remove("keerthi")
 print(names)
 cars.remove("BMW")
 print(cars)
+
+def mergeSort(left, right):
+    result=[]
+    i,j=0,0
+    while i<len(left) and j<len(right):
+        if left[i]<=right[j]:
+            result.append(left[i])
+            i+=1
+        else:
+            result.append(right[j])
+            j+=1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+def merge_sort(arr):
+    if len(arr)<=1:
+        return arr
+    mid=len(arr)//2
+    left=merge_sort(arr[:mid])
+    right=merge_sort(arr[mid:])
+    return mergeSort(left,right)
+arr=[4,5,3,2,9]
+k=2
+sorted_arr=merge_sort(arr)
+print(merge_sort)
+print(f"{k}nd smallest element:", sorted_arr[k - 1])
