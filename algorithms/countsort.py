9a7c663d570e3223d7e88f3608b4e4e0c5b6b3c2
keerthi <muchhakeerthi@gmail.com>
2026-07-07 22:40:54 +0530

    added

--- algorithms/countsort.py
+++ algorithms/countsort.py
@@ -1,20 +1,28 @@
 def counting_sort(arr):
     maximum = max(arr)
-
     count = [0] * (maximum + 1)
-
     # Count frequencies
     for num in arr:
         count[num] += 1
-
     # Build sorted array
     sorted_arr = []
-
     for i in range(len(count)):
         sorted_arr.extend([i] * count[i])
-
     return sorted_arr
-
-
 arr = [4, 2, 2, 8, 3, 3, 1]
 print(counting_sort(arr)) 
+
+def counting_sort(arr): # arr = [4,2,2,8,3,3,1] 
+    maximum=max(arr) #  8
+    count=[0] * (maximum+1) #[0 0 0 0 0 0 0 0]
+    for num in arr:
+        count[num]+=1
+    j=0
+    for i in range(len(count)):
+        while count[i]>0:
+            arr[j]=i
+            j+=1
+            count[i]-=1
+    return arr
+a=[4, 2, 2, 8, 3, 3, 1]
+print(counting_sort(a))
