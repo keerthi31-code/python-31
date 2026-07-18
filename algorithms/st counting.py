97ffd7d1ce09136098cb2dec4fc270a80d254437
keerthi <muchhakeerthi@gmail.com>
2026-07-18 23:01:01 +0530

    added

--- algorithms/st counting.py
+++ algorithms/st counting.py
@@ -38,5 +38,25 @@ def counting_sort(arr):
 
 arr = [4,2,2,8,3,3,1]
 print(counting_sort(arr))
-print('keerthi')
-print("navya")
+
+
+def counting_sort(arr):
+    maximum = max(arr)
+    count = [0] * (maximum + 1)   #creating count array
+
+    for num in arr:
+        count[num] += 1   #count frequencies
+    for i in range(1, len(count)):
+        count[i] += count[i-1]
+    output = [0] * len(arr)
+
+    for i in range(len(arr)-1, -1, -1):
+        num = arr[i]
+        output[count[num]-1] = num
+        count[num] -= 1
+
+    return output
+
+arr = [8,4,3,5,5,8,2,1]
+print(counting_sort(arr))
+
