06807c664f8214d21669825b3ebf522f30242a7e
keerthi <muchhakeerthi@gmail.com>
2026-07-20 20:30:53 +0530

    added

--- TWOPOINTERS.py
+++ TWOPOINTERS.py
@@ -102,3 +102,13 @@ def two_sum(arr, target):
 arr=[2,4,6,8,10]
 target=6
 print(two_sum(arr,target))
+
+def move_zeroes(arr):
+    pos=0
+    for i in range(len(arr)):
+        if arr[i]!=0:
+            arr[i],arr[pos]=arr[pos],arr[i]
+            pos+=1
+    return arr
+arr=[1,0,3,0,5,6]
+print(move_zeroes(arr))
