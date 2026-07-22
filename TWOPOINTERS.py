8025cbdbb7a0680a0187280697661d9be5e5e524
keerthi <muchhakeerthi@gmail.com>
2026-07-22 23:30:46 +0530

    added

--- TWOPOINTERS.py
+++ TWOPOINTERS.py
@@ -112,7 +112,7 @@ def move_zeroes(arr):
     return arr
 arr=[1,0,3,0,5,6]
 print(move_zeroes(arr))
-
+'''
 def remove_duplicates(arr):
     res=[]
     left=0
@@ -126,14 +126,31 @@ def remove_duplicates(arr):
         right-=1
 arr=[1,2,1,4,5,6,6,7,8]
 print(remove_duplicates(arr))
+'''
 
-
-
-def prime_num(n):
-    for i in n:
-        if i%2==0:
-            return False
+def merge_arrs(arr1,arr2):
+    i=0
+    j=0
+    res=[]
+    while i<(len(arr1)) and j<len(arr2):
+        if arr1[i]<arr2[j]:
+            res.append(arr1[i])
+            i+=1
         else:
-            return n
-print(prime_num(30))
-        
+            res.append(arr2[j])
+            j+=1
+    while i<len(arr1):
+        res.append(arr1[i])
+        i+=1
+    while j<len(arr2):
+        res.append(arr2[j])
+        j+=1
+    return res
+
+arr1=[1,3,5]
+arr2=[2,4,6]
+print(merge_arrs(arr1,arr2))
+
+
+
+
