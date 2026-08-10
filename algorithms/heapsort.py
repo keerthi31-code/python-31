fbcc447911d0b38c255fa8a5b8f91d5effc0283c
keerthi <muchhakeerthi@gmail.com>
2026-08-10 23:40:15 +0530

    Added

---
+++ algorithms/heapsort.py
@@ -0,0 +1,11 @@
+# heap queue a data structure commonly used to implement a prority queue efficient
+#priority is process the elements to its prority queue
+import heapq
+a=[]
+heapq.heappush(a,10)
+heapq.heappush(a,5)
+heapq.heappush(a,25)
+heapq.heappush(a,7)
+heapq.heappush(a,9)
+print(a)
+print(a[0])
