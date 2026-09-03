9c20bbb0d271adedc77fc68a61e052ea45c13ec2
keerthi <muchhakeerthi@gmail.com>
2026-09-03 22:50:34 +0530

    Added

--- prob.py
+++ prob.py
@@ -639,6 +639,7 @@ def bignum(arr):
 arr=[4,1,7,3]
 print(bignum(arr))  
 
+
 graph={}
 n=int(input("enter no of nodes: "))
 for i in range(n):
@@ -654,4 +655,19 @@ for j in range(t):
 print("adjacency list:")
 for k in graph:
     print(k,":",graph[k])
-print("keerthi")
+
+graph={}
+n=int(input("enter no of nodes: "))
+for i in range(n):
+    node=input("enter node:")
+    graph[node]=[]
+t=int(input("enter the no. of edges: "))
+for j in range(t):
+    print("enter edge:",j+1)
+    n1=input("enter 1st node:")
+    n2=input("enter 2nd node:")
+    graph[n1].append(n2)
+    graph[n2].append(n1)
+print("adjacency list:")
+for k in graph:
+    print(k,":",graph[k])
